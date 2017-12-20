06dfd7e018ef04cfa4deb45a1029603c78cf9a5e
shekinahdalayya <khcy6sdd@nottingham.edu.my>
2017-12-20 16:59:57 +0800

    no arrival time

--- src/main/FCFS.java
+++ src/main/FCFS.java
@@ -9,7 +9,6 @@ class FCFS
 	public static void main(String args[]) throws Exception 
 	{ 
 		//n is number of processes
-		//at is arrival time
 		//bt is burst time
 		//wt is waiting time
 		//tat is turn around/arrival time
@@ -31,7 +30,6 @@ class FCFS
 		wt=new int[n]; 
 		tat=new int[n]; 
 		bt=new int[n];
-		at=new int[n];
 
 		
 		//Each burst time is stored in bt[]
@@ -42,37 +40,31 @@ class FCFS
 			bt[i]=Integer.parseInt(br.readLine()); 
 		} 
 		
-		//Each around time is stored in at[]
-		System.out.println("\n\nEnter Around Time"); 
-		for(int i=0;i<n;i++) 
-		{ 
-			System.out.println("Process["+i+"]"); 
-			at[i]=Integer.parseInt(br.readLine()); 
-		} 
-		
 		System.out.println("\n"); 
 		
 		//Waiting time for the first process is 0
 		wt[0]=0;
 		
-		//Calculation of turnaround time
-		for(int i=0;i<n;i++) 
-		{ 	
-			ct+=bt[i];
-			tat[i]=ct-at[i];
-		} 
-		
-		//Calculation of waiting time and total waiting time
+		//Calculation of turnaround time, waiting time and total waiting time
+		for(int i=0;i<n;i++){
+			wt[i]=0;
+			tat[i]=0;
+		}
 		for(int i=1;i<n;i++) 
 		{ 
-			wt[i]=tat[i]-bt[i];
-			awt=awt+wt[i]; 
+			wt[i]=wt[i-1]+bt[i-1];  
 		} 
+		for(int i=0;i<n;i++) 
+		{ 
+			tat[i]=wt[i]+bt[i]; 
+			awt=awt+wt[i]; 
 
-		System.out.println(" PROCESS\tARRIVAL TIME\tBURST-TIME\tWAITING-TIME\tTURN AROUND-TIME\n"); 
+		} 
+	
+		System.out.println(" PROCESS\tBURST-TIME\tWAITING-TIME\tTURN AROUND-TIME\n"); 
 		for(int i=0;i<n;i++) 
 		{
-			System.out.println("   "+ i + "\t\t"+ at[i] + "\t\t"+bt[i]+"\t\t"+wt[i]+"\t\t"+tat[i]);
+			System.out.println("   "+ i + "\t\t"+bt[i]+"\t\t"+wt[i]+"\t\t"+tat[i]);
 		} 
 		
 		//calculation of total turnaround time
